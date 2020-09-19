ca2f8cc5b2ef67646d23fd9f68fb809ca20d008b
Mahdi Hasani <mahdi.hasani@chargoon.net>
2020-09-19 08:35:56 +0430

    Table Product

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,8 +1,29 @@
+import { Fab } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip/Tooltip';
 import React from 'react';
 import { Table } from './Components';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        fab: {
+            marginTop: theme.spacing(2),
+            marginBottom: theme.spacing(2),
+        },
+    }),
+);
 
 const Product = () => {
-    return <Table />
+    const classes = useStyles();
+    return <div>
+        <Tooltip title="اضافه نمودن محصول جدید" aria-label="add">
+            <Fab color="secondary" className={classes.fab}>
+                <AddIcon />
+            </Fab>
+        </Tooltip>
+        <Table />
+    </div>
 }
 
 export default Product;
