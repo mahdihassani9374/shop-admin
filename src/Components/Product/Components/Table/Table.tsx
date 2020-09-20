7242b96b8e0a0dc62eb81e38b25336bf86730f0e
Mahdi Hasani <mahdi.hasani@chargoon.net>
2020-09-20 15:16:06 +0430

    pagination

--- src/Components/Product/Components/Table/Table.tsx
+++ src/Components/Product/Components/Table/Table.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,12 +12,26 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import Tooltip from '@material-ui/core/Tooltip/Tooltip';
 import Img from './../../../../Assets/Img/product.jpg';
 import Avatar from '@material-ui/core/Avatar';
+import TableFooter from '@material-ui/core/TableFooter';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import IconButton from '@material-ui/core/IconButton';
+import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    table: {
+      minWidth: 650,
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
 
 interface IProduct {
   name: string,
@@ -41,6 +55,12 @@ const rows: IProduct[] = [
 
 export default function SimpleTable() {
   const classes = useStyles();
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setAge(event.target.value as string);
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -76,6 +96,32 @@ export default function SimpleTable() {
             </TableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow style={{display:'flex',alignItems:'center'}}>
+            <IconButton component="span">
+              <ChevronRightIcon />
+            </IconButton>
+            <IconButton disabled component="span">
+              <ChevronLeftIcon />
+            </IconButton>
+            <span>
+              نمایش  1 تا 12  از 130
+            </span>
+            <FormControl variant="filled" className={classes.formControl}>
+              <InputLabel id="demo-simple-select-filled-label">تعداد نمایشی</InputLabel>
+              <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                value={age}
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+              </Select>
+            </FormControl>
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
